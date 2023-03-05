edcae7b14c196f512912563ab30202a182fe8175
Ronny Barahona <ronnybarahona10@gmail.com>
2023-03-05 16:37:53 +0000

    hmm

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -6,8 +6,12 @@ public class ArraySum {
      * @param arr an array of ints
      * @return the sum of all values in arr.
      */
-    public int sum(int[] arr){
-
-        return 0;
+    public int sum(int[] arr) {
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        return total;
     }
+    
 }
